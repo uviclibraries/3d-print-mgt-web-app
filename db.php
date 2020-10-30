bf539ccdd01b2e6c18ceb9e09deddf9871630176
e1znek <35316062+e1znek@users.noreply.github.com>
2020-10-30 16:15:45 -0700

    browser db deatials

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
-$servername = "3d_print_jobs";
+//$servername = "3d_print_jobs";
 $username = "3d_print";
 $password = "Hx_27!PfQ";
+$dbname = "3d_print_jobs";
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=3d_print_mgt", $username, $password);
+  $conn = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   #echo “Connected successfully”;
