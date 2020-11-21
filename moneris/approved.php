896a71e66b6d45a80a8216f835d9c4d9488a4b46
e1znek <35316062+e1znek@users.noreply.github.com>
2020-11-20 17:45:50 -0800

    update print_job
    still need to save payment data to moneris_fields

--- moneris/approved.php
+++ moneris/approved.php
@@ -4,12 +4,10 @@
   //               -  Moneris returns user to this page on approved payment
   //
   session_start();
+  include '../auth-sec.php';
   include 'moneris.inc.php';
 
 
-  $job_id = $_SESSION['id'];
-  $user_name = $_SESSION['user_name'];
-  $user_email = $_SESSION['user_email'];
 ?>
 <!doctype html>
 <html lang="en">
@@ -31,6 +29,8 @@ The complete Moneris transaction response fields are:
 </p>
 <table>
 <?php
+  $input   = array();
+  $columns = implode(", ",$moneris_response_fields);
   foreach ($moneris_response_fields as $field) {
     if (array_key_exists($field, $_POST)) {
       $value = $_POST[$field];
@@ -38,15 +38,32 @@ The complete Moneris transaction response fields are:
     else {
       $value = '';
     }
+    $input[] = $value;
     echo "<tr><td>$field</td><td>$value</td></tr>\n";
   }
+$inputStr = implode(", ", $input);
+
+/* need to add to moneris_fields
+$stm = $conn->prepare("INSERT INTO moneris_fields (".$columns.") VALUES ('$inputStr')");
+$stm->execute();
+*/
+
+//Change Status
+$current_date = date("Y-m-d");
+$current_status = "ready_to_print";
+$stmt = $conn->prepare("UPDATE print_job SET status = :status, ready_to_prnt_date = :rdy WHERE id = :job_id");
+$stmt->bindParam(':status', $current_status);
+$stmt->bindParam(':rdy', $current_date);
+$stmt->bindParam(':job_id', $_SESSION['job_id']);
+$stmt->execute();
+
+
 ?>
 </table>
 <p>
 This data must be stored in the database so the admin user(s) can review
 it if/when required.
 </p>
-
 <a href="../customer-dashboard.php">
 <button type="button" type="submit">Return</button>
 </a>
