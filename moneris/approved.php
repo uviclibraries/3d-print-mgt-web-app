820a94fb15bc6ad28f10223fa8a5c778f803ce1d
e1znek <35316062+e1znek@users.noreply.github.com>
2020-11-26 19:32:08 -0800

    Updates moneris fields
    Still need to clean up presentation

--- moneris/approved.php
+++ moneris/approved.php
@@ -39,16 +39,10 @@ $stmt->bindParam(':rdy', $current_date);
 $stmt->bindParam(':job_id', $_SESSION['job_id']);
 $stmt->execute();
 
-echo "post";
-foreach ($_POST as $key=>$result) {
-  echo $key. " >--holds--> " . $result;
-  ?>
-  <br>
-  <?php
-}
 
+
+//Fill array with returned values.
   $input   = array();
-  $columns = implode(", ",$moneris_response_fields);
   foreach ($moneris_response_fields as $field) {
     if (array_key_exists($field, $_POST)) {
       $value = $_POST[$field];
@@ -58,24 +52,38 @@ foreach ($_POST as $key=>$result) {
     }
     echo "<tr><td>$field</td><td>$value</td></tr>\n";
     if ($value == '' OR $value == "" OR $value == NULL) {
-      $input[] = "NULL";
+      $input[$field] = "NULL";
     }else{
-      $input[] = $value;
+      $input[$field] = $value;
     }
   }
-$inputStr = implode(", ", $input);
 
-?>
-<p><?php echo $columns ?> </p>
-<p> <?php echo $inputStr ?> </p>
-
-<?php
+$escaped_values = array_map('addslashes', $input);
 //add to moneris_fields
-/*
-$sql = "INSERT INTO moneris_fields  " . $columns ." VALUES " . $inputStr;
-$stm = $conn->prepare($sql);
+$stm = $conn->prepare("INSERT INTO moneris_fields (response_order_id,response_code, date_stamp, time_stamp, result, trans_name, cardholder, card, charge_total, f4l4, message, iso_code, bank_approval_code, bank_transaction_id, txn_num, avs_response_code, cavv_result_code, INVOICE, ISSCONF, ISSNAME) VALUES (:response_order_id, :response_code, :date_stamp, :time_stamp, :result, :trans_name, :cardholder, :card, :charge_total, :f4l4, :message, :iso_code, :bank_approval_code, :bank_transaction_id, :txn_num, :avs_response_code, :cavv_result_code, :INVOICE, :ISSCONF, :ISSNAME)");
+//
+$stm->bindParam(':response_order_id', $input['response_order_id']);
+$stm->bindParam(':response_code', $input['response_code']);
+$stm->bindParam(':date_stamp', $input['date_stamp']);
+$stm->bindParam(':time_stamp', $input['time_stamp']);
+$stm->bindParam(':result', $input['result']);
+$stm->bindParam(':trans_name', $input['trans_name']);
+$stm->bindParam(':cardholder', $input['cardholder']);
+$stm->bindParam(':card', $input['card']);
+$stm->bindParam(':charge_total', $input['charge_total']);
+$stm->bindParam(':f4l4', $input['f4l4']);
+$stm->bindParam(':message', $input['message']);
+$stm->bindParam(':iso_code', $input['iso_code']);
+$stm->bindParam(':bank_approval_code', $input['bank_approval_code']);
+$stm->bindParam(':bank_transaction_id', $input['bank_transaction_id']);
+$stm->bindParam(':txn_num', $input['txn_num']);
+$stm->bindParam(':avs_response_code', $input['avs_response_code']);
+$stm->bindParam(':cavv_result_code', $input['cavv_result_code']);
+$stm->bindParam(':INVOICE', $input['INVOICE']);
+$stm->bindParam(':ISSCONF', $input['ISSCONF']);
+$stm->bindParam(':ISSNAME', $input['ISSNAME']);
 $stm->execute();
-*/
+
 
 ?>
 </table>
--- moneris/declined.php
+++ moneris/declined.php
@@ -33,9 +33,9 @@ foreach ($_POST as $key=>$result) {
   <br>
   <?php
 }
-$input   = array();
-$columns = implode(", ",$moneris_response_fields);
 
+//Fill array with returned values.
+$input   = array();
   foreach ($moneris_response_fields as $field) {
     if (array_key_exists($field, $_POST)) {
       $value = $_POST[$field];
@@ -45,25 +45,42 @@ $columns = implode(", ",$moneris_response_fields);
     }
     echo "<tr><td>$field</td><td>$value</td></tr>\n";
     if ($value == '' OR $value == "" OR $value == NULL) {
-      $input[] = "NULL";
+      $input[$field] = "NULL";
     }else{
-      $input[] = $value;
+      $input[$field] = $value;
     }
   }
 
-/*
-$inputStr = implode(", ", $input);
-$sqlStr = 'INSERT INTO moneris_fields (response_order_id, response_code, date_stamp, time_stamp, result, trans_name, cardholder, card, charge_total, f4l4, message, iso_code, bank_approval_code, bank_transaction_id, txn_num, avs_response_code, cavv_result_code, INVOICE, ISSCONF, ISSNAME) VALUES (' . $inputStr . ')';
-$field_results = $conn->prepare($sqlStr);
-$field_results->execute();
-*/
+  //add to moneris_fields
+  $stm = $conn->prepare("INSERT INTO moneris_fields (response_order_id,response_code, date_stamp, time_stamp, result, trans_name, cardholder, card, charge_total, f4l4, message, iso_code, bank_approval_code, bank_transaction_id, txn_num, avs_response_code, cavv_result_code, INVOICE, ISSCONF, ISSNAME) VALUES (:response_order_id, :response_code, :date_stamp, :time_stamp, :result, :trans_name, :cardholder, :card, :charge_total, :f4l4, :message, :iso_code, :bank_approval_code, :bank_transaction_id, :txn_num, :avs_response_code, :cavv_result_code, :INVOICE, :ISSCONF, :ISSNAME)");
+  //Bind variables
+  $stm->bindParam(':response_order_id', $input['response_order_id']);
+  $stm->bindParam(':response_code', $input['response_code']);
+  $stm->bindParam(':date_stamp', $input['date_stamp']);
+  $stm->bindParam(':time_stamp', $input['time_stamp']);
+  $stm->bindParam(':result', $input['result']);
+  $stm->bindParam(':trans_name', $input['trans_name']);
+  $stm->bindParam(':cardholder', $input['cardholder']);
+  $stm->bindParam(':card', $input['card']);
+  $stm->bindParam(':charge_total', $input['charge_total']);
+  $stm->bindParam(':f4l4', $input['f4l4']);
+  $stm->bindParam(':message', $input['message']);
+  $stm->bindParam(':iso_code', $input['iso_code']);
+  $stm->bindParam(':bank_approval_code', $input['bank_approval_code']);
+  $stm->bindParam(':bank_transaction_id', $input['bank_transaction_id']);
+  $stm->bindParam(':txn_num', $input['txn_num']);
+  $stm->bindParam(':avs_response_code', $input['avs_response_code']);
+  $stm->bindParam(':cavv_result_code', $input['cavv_result_code']);
+  $stm->bindParam(':INVOICE', $input['INVOICE']);
+  $stm->bindParam(':ISSCONF', $input['ISSCONF']);
+  $stm->bindParam(':ISSNAME', $input['ISSNAME']);
+  $stm->execute();
 ?>
 </table>
 <p>
 This data must be stored in the database so the admin user(s) can review
 it if/when required. hello.
 </p>
-<p> <?php echo $inputStr ?> </p>
 
 <a href="../customer-dashboard.php">
 <button type="button" type="submit">Return</button>
