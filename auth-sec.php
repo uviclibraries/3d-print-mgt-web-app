0cb7adabc99b76f8b15ea4b35dc07d21b4dcb006
e1znek <35316062+e1znek@users.noreply.github.com>
2020-11-27 16:31:25 -0800

    Authentication swap
    auth-sec checks if log in. if not redirects to index. index holds CASPHP and does the login and session set, before redirecting to cust-dash

--- auth-sec.php
+++ auth-sec.php
@@ -1,69 +1,13 @@
 <?php
-//
-//  phpCAS/index.php  -  CAS authentication using phpCAS module
-//                    -  based on:  https://github.com/apereo/phpCAS/blob/master/docs/examples/example_simple.php
-//
-require_once 'CAS.php'; //required for uvic CASphp
 require ('db.php');			//required to access db
 
-//"Global" Variables
-$user = "";	//user netlink_id
-$user_email = "@uvic.ca";
-$user_type = 1; //Regular
-$user_name = "non-admin";
-
-//Not signed in, no session itmes.
+//Check if user is logged in
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
-	phpCAS::setDebug();
-	phpCAS::setVerbose(true);
-
-	phpCAS::client(CAS_VERSION_2_0, 'www.uvic.ca', 443, '/cas');
-	phpCAS::setNoCasServerValidation();
-	phpCAS::forceAuthentication();
-
-
-	$user = phpCAS::getUser();
-	$usersearch = $conn->query("SELECT name, user_type, email FROM users WHERE netlink_id = '$user'");
-	//if user is not in the db create entry for new user.
-	if ($usersearch->rowCount() < 1) {
-
-		//LDAP Start
-		require('ldap.inc.php');
-		list($user_name, $user_email, $error) = get_personal_info($user);
-		if ($error) {
-			//Do this if error occurs.
-	    $redirect_url = 'error.php?mesg=' . urlencode($error);
-	    header("Location: $redirect_url");
-	    exit;
-	  }
-		//LDAP end
-
-		//Insert into db
-		$stm = $conn->prepare("INSERT INTO users (netlink_id, name, user_type, email) VALUES (:netlink_id, :name, :user_type, :email)");
-		$stm->bindParam(':netlink_id', $user);
-		$stm->bindParam(':name', $user_name);
-		$user_type = 1;
-		$stm->bindParam(':user_type', $user_type);
-		$stm->bindParam(':email', $user_email);
-		$stm->execute();
-	}
-	//user exists in db
-	else {
-		foreach ($usersearch as $key) {
-			$_SESSION['user'] = $user;
-			$_SESSION['user_name'] = $key["name"];
-		  $_SESSION['user_email'] = $key["email"];
-			$_SESSION['user_type'] = $key["user_type"];
-		}
-	}
-	$_SESSION['loggedin'] = true;
-	$getUserID = $conn->query("SELECT id FROM users WHERE netlink_id = '$user'");
-	foreach ($getUserID as $key) {
-		$_SESSION['user_id'] = $key['id'];
-	}
+	//go to login page.
+	header("Location: index.php");
 }
-//user = loggedin
-//set session variables
+	
+//get session variables
 $user = $_SESSION['user'];
 $user_name =$_SESSION['user_name'];
 $user_email = $_SESSION['user_email'];
@@ -71,12 +15,12 @@ $user_type = $_SESSION['user_type'];
 
 // logout if desired
 if (isset($_REQUEST['logout'])) {
+	require_once 'CAS.php'; //required for uvic CASphp
 	$_SESSION['loggedin'] = false;
-	phpCAS::client(CAS_VERSION_2_0, 'www.uvic.ca', 443, '/cas');
-	phpCAS::logout();
 	session_unset();
 	session_destroy();
+	phpCAS::client(CAS_VERSION_2_0, 'www.uvic.ca', 443, '/cas');
+	phpCAS::logout();
 }
-
 
 ?>
--- index.php
+++ index.php
@@ -1,110 +1,69 @@
 <?php
+//
+//  phpCAS/index.php  -  CAS authentication using phpCAS module
+//                    -  based on:  https://github.com/apereo/phpCAS/blob/master/docs/examples/example_simple.php
+//
 session_start();
-require ('auth-sec.php');
+require_once 'CAS.php'; //required for uvic CASphp
+require ('db.php');			//required to access db
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+	phpCAS::setDebug();
+	phpCAS::setVerbose(true);
+
+	phpCAS::client(CAS_VERSION_2_0, 'www.uvic.ca', 443, '/cas');
+	phpCAS::setNoCasServerValidation();
+	phpCAS::forceAuthentication();
+
+
+	$user = phpCAS::getUser();
+	$usersearch = $conn->query("SELECT name, user_type, email FROM users WHERE netlink_id = '$user'");
+	//if user is not in the db create entry for new user.
+	if ($usersearch->rowCount() < 1) {
+
+		//LDAP Start
+		require('ldap.inc.php');
+		list($user_name, $user_email, $error) = get_personal_info($user);
+		if ($error) {
+			//Do this if error occurs.
+	    $redirect_url = 'error.php?mesg=' . urlencode($error);
+	    header("Location: $redirect_url");
+	    exit;
+	  }
+		//LDAP end
+
+		//Insert into db
+		$stm = $conn->prepare("INSERT INTO users (netlink_id, name, user_type, email) VALUES (:netlink_id, :name, :user_type, :email)");
+		$stm->bindParam(':netlink_id', $user);
+		$stm->bindParam(':name', $user_name);
+		$user_type = 1;
+		$stm->bindParam(':user_type', $user_type);
+		$stm->bindParam(':email', $user_email);
+		$stm->execute();
+    //add session variables
+    $_SESSION['user'] = $user;
+    $_SESSION['user_name'] = $user_name;
+    $_SESSION['user_email'] = $user_email;
+    $_SESSION['user_type'] = 1;
+	}
+	//user exists in db
+	else {
+		foreach ($usersearch as $key) {
+      //create session variables
+			$_SESSION['user'] = $user;
+			$_SESSION['user_name'] = $key["name"];
+		  $_SESSION['user_email'] = $key["email"];
+			$_SESSION['user_type'] = $key["user_type"];
+		}
+	}
+	$_SESSION['loggedin'] = true;
+  //get user_id
+	$getUserID = $conn->query("SELECT id FROM users WHERE netlink_id = '$user'");
+	foreach ($getUserID as $key) {
+		$_SESSION['user_id'] = $key['id'];
+	}
+}
+
+//redirects to dashboard
 header("Location: customer-dashboard.php");
 # UVic Libraries Header for navigation purposes would go here.
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v4.0.1">
-    <title>Welcome to the DSC 3D print page</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/checkout/">
-
-    <!-- Bootstrap core CSS -->
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <!-- Favicons -->
-<link rel="apple-touch-icon" href="/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="manifest" href="/docs/4.5/assets/img/favicons/manifest.json">
-<link rel="mask-icon" href="/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon.ico">
-<meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
-<meta name="theme-color" content="#563d7c">
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-      html,
-body {
-  height: 100%;
-}
-
-body {
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-align: center;
-  align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  background-color: #f5f5f5;
-}
-
-.form-signin {
-  width: 100%;
-  max-width: 330px;
-  padding: 15px;
-  margin: auto;
-}
-.form-signin .checkbox {
-  font-weight: 400;
-}
-.form-signin .form-control {
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-    </style>
-    <!-- Custom styles for this template -->
-
-  </head>
-  <body class="text-center">
-    <form class="form-signin">
-
-  <h1 class="h3 mb-3 font-weight-normal">DSC 3D Print sign in</h1>
-  <path fill-rule="evenodd" d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094L15.002 9.5V13a1 1 0 0 1-1 1h-12a1 1 0 0 1-1-1v-1zm5-6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-</svg>
-
-  <a class="btn btn-primary btn-lg btn-block" href="customer-dashboard.php" role="button">Dashboard</a>
-  <p>Hello user: "<i><?php echo phpCAS::getUser(); ?></i>" Here I will talk about what this is and how it works. FAQ page link will also be on this page.</p>
-
-  <a class="btn btn-primary btn-lg btn-block" href="?logout=" role="button">Logout</a>
-
-  <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
-</form>
-</body>
-</html>
