401d3b2409d75ce64c4d33d22a942b2b83a0b6bf
e1znek <35316062+e1znek@users.noreply.github.com>
2021-01-12 11:25:04 -0800

    Adds netlink/name to moneris fields
    Also reformated

--- moneris/approved.php
+++ moneris/approved.php
@@ -7,7 +7,74 @@
   include '../db.php';
   include 'moneris.inc.php';
 
+  //Get moneris responses
+  $input   = array();
+  foreach ($moneris_response_fields as $field) {
+    if (array_key_exists($field, $_POST)) {
+      $value = $_POST[$field];
+    }
+    else {
+      $value = '';
+    }
+    if ($value == '' OR $value == "" OR $value == NULL) {
+      $input[$field] = "NULL";
+    }else{
+      $input[$field] = $value;
+    }
+  }
 
+
+
+  //Split into:
+  //0: netlinkid
+  //1: full name(after search)
+  //2: job_id
+  $job_id = explode("-", $input['response_order_id']);
+
+  //Change Status
+  $current_date = date("Y-m-d");
+  $current_status = "paid";
+  $stmt = $conn->prepare("UPDATE print_job SET status = :status, paid_date = :paid_date WHERE id = :job_id");
+  $stmt->bindParam(':status', $current_status);
+  $stmt->bindParam(':paid_date', $current_date);
+  $stmt->bindParam(':job_id', $job_id[2]);
+  $stmt->execute();
+
+  //get full name
+  $stmt = $conn->prepare("SELECT name FROM users WHERE netlink_id = :netlink_id");
+  $stmt->bindParam(':netlink_id', $job_id[0]);
+  $stmt->execute();
+  $getnetlink = $stmt->fetchAll();
+  foreach ($getnetlink as $key) {
+    $job_id[1] = $key["name"];
+  }
+
+  //add to moneris_fields
+  $stm = $conn->prepare("INSERT INTO moneris_fields (response_order_id, netlink_id, full_name, response_code, date_stamp, time_stamp, result, trans_name, cardholder, card, charge_total, f4l4, message, iso_code, bank_approval_code, bank_transaction_id, txn_num, avs_response_code, cavv_result_code, INVOICE, ISSCONF, ISSNAME) VALUES (:response_order_id, :netlink_id, :full_name, :response_code, :date_stamp, :time_stamp, :result, :trans_name, :cardholder, :card, :charge_total, :f4l4, :message, :iso_code, :bank_approval_code, :bank_transaction_id, :txn_num, :avs_response_code, :cavv_result_code, :INVOICE, :ISSCONF, :ISSNAME)");
+
+  $stm->bindParam(':response_order_id', $input['response_order_id']);
+  $stm->bindParam(':netlink_id', $job_id[0]);
+  $stm->bindParam(':full_name', $job_id[1]);
+  $stm->bindParam(':response_code', $input['response_code']);
+  $stm->bindParam(':date_stamp', $input['date_stamp']);
+  $stm->bindParam(':time_stamp', $input['time_stamp']);
+  $stm->bindParam(':result', $input['result']);
+  $stm->bindParam(':trans_name', $input['trans_name']);
+  $stm->bindParam(':cardholder', $input['cardholder']);
+  $stm->bindParam(':card', $input['card']);
+  $stm->bindParam(':charge_total', $input['charge_total']);
+  $stm->bindParam(':f4l4', $input['f4l4']);
+  $stm->bindParam(':message', $input['message']);
+  $stm->bindParam(':iso_code', $input['iso_code']);
+  $stm->bindParam(':bank_approval_code', $input['bank_approval_code']);
+  $stm->bindParam(':bank_transaction_id', $input['bank_transaction_id']);
+  $stm->bindParam(':txn_num', $input['txn_num']);
+  $stm->bindParam(':avs_response_code', $input['avs_response_code']);
+  $stm->bindParam(':cavv_result_code', $input['cavv_result_code']);
+  $stm->bindParam(':INVOICE', $input['INVOICE']);
+  $stm->bindParam(':ISSCONF', $input['ISSCONF']);
+  $stm->bindParam(':ISSNAME', $input['ISSNAME']);
+  $stm->execute();
 ?>
 <!doctype html>
 <html lang="en">
@@ -37,73 +104,6 @@
 <p>
 Your payment has been approved.  A receipt should have been sent from Moneris.
 </p>
-<p>
-The complete Moneris transaction response fields are:
-</p>
-<table>
-<?php
-
-
-
-//Fill array with returned values.
-$input   = array();
-foreach ($moneris_response_fields as $field) {
-  if (array_key_exists($field, $_POST)) {
-    $value = $_POST[$field];
-  }
-  else {
-    $value = '';
-  }
-  echo "<tr><td>$field</td><td>$value</td></tr>\n";
-  if ($value == '' OR $value == "" OR $value == NULL) {
-    $input[$field] = "NULL";
-  }else{
-    $input[$field] = $value;
-  }
-}
-
-
-//Change Status
-$current_date = date("Y-m-d");
-$current_status = "paid";
-$job_id = explode("-", $input['response_order_id']);
-
-$stmt = $conn->prepare("UPDATE print_job SET status = :status, paid_date = :paid_date WHERE id = :job_id");
-$stmt->bindParam(':status', $current_status);
-$stmt->bindParam(':paid_date', $current_date);
-$stmt->bindParam(':job_id', $job_id[2]);  //taken from order_id. any better way?
-$stmt->execute();
-
-
-
-
-$escaped_values = array_map('addslashes', $input);
-//add to moneris_fields
-$stm = $conn->prepare("INSERT INTO moneris_fields (response_order_id,response_code, date_stamp, time_stamp, result, trans_name, cardholder, card, charge_total, f4l4, message, iso_code, bank_approval_code, bank_transaction_id, txn_num, avs_response_code, cavv_result_code, INVOICE, ISSCONF, ISSNAME) VALUES (:response_order_id, :response_code, :date_stamp, :time_stamp, :result, :trans_name, :cardholder, :card, :charge_total, :f4l4, :message, :iso_code, :bank_approval_code, :bank_transaction_id, :txn_num, :avs_response_code, :cavv_result_code, :INVOICE, :ISSCONF, :ISSNAME)");
-//
-$stm->bindParam(':response_order_id', $input['response_order_id']);
-$stm->bindParam(':response_code', $input['response_code']);
-$stm->bindParam(':date_stamp', $input['date_stamp']);
-$stm->bindParam(':time_stamp', $input['time_stamp']);
-$stm->bindParam(':result', $input['result']);
-$stm->bindParam(':trans_name', $input['trans_name']);
-$stm->bindParam(':cardholder', $input['cardholder']);
-$stm->bindParam(':card', $input['card']);
-$stm->bindParam(':charge_total', $input['charge_total']);
-$stm->bindParam(':f4l4', $input['f4l4']);
-$stm->bindParam(':message', $input['message']);
-$stm->bindParam(':iso_code', $input['iso_code']);
-$stm->bindParam(':bank_approval_code', $input['bank_approval_code']);
-$stm->bindParam(':bank_transaction_id', $input['bank_transaction_id']);
-$stm->bindParam(':txn_num', $input['txn_num']);
-$stm->bindParam(':avs_response_code', $input['avs_response_code']);
-$stm->bindParam(':cavv_result_code', $input['cavv_result_code']);
-$stm->bindParam(':INVOICE', $input['INVOICE']);
-$stm->bindParam(':ISSCONF', $input['ISSCONF']);
-$stm->bindParam(':ISSNAME', $input['ISSNAME']);
-$stm->execute();
-?>
-</table>
 <br>
 <a href="../customer-dashboard.php">
 <button type="button" type="submit">Return</button>
