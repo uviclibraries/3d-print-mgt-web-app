7a638b1218894afc3c431bca70adba3ee13d6712
e1znek <35316062+e1znek@users.noreply.github.com>
2021-01-28 13:49:00 -0800

    Fixed search for proper results

--- jobs_cron/semester_delete.php
+++ jobs_cron/semester_delete.php
@@ -4,15 +4,15 @@
 require ('db.php');
 
 //One semester ago & one semester + 2 weeks ago.
-$today_120 = date("Y-m-d", strtotime(-120 days));
-$today_134 = date("Y-m-d", strtotime(-134 days));
+$today_120 = date('Ymd', strtotime("-120 days"));
+$today_134 = date('Ymd', strtotime("-134 days"));
 
 //Archived jobs
-$stm = $conn->query("SELECT model_name, model_name_2 FROM print_job WHERE status = 'archived' AND completed_date <= $today_120 AND completed_date >= $today_134");
+$stm = $conn->query("SELECT model_name, model_name_2 FROM print_job WHERE status = 'archived' AND (completed_date BETWEEN $today_134 AND $today_120)");
 $job_arh = $stm->fetchAll();
+
 foreach ($job_arh as $job) {
   $delete = "uploads/" . $job['model_name'];
-
   if (is_file($delete)) {
     unlink($delete);
   }
