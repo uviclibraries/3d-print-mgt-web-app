042d83e35cea3d8095a798fbb77df3fb4a4ad524
Rich McCue <rich@msys.ca>
2023-11-09 14:30:54 -0800

    Moneris checkin

--- moneris/customer-payment.php
+++ moneris/customer-payment.php
@@ -3,62 +3,71 @@ session_start();
 include '../auth-sec.php'; //Gets CAS & db
 include 'moneris.inc.php';
 
-
-
-
 $order_id = $user ."-". date('YmdHis') ."-". $_SESSION['job_id'];
-//
-// initiate Moneris HPP preload request
-//
-$post_fields= [
-    'ps_store_id' => $moneris_store_id,
-    'hpp_key' => $moneris_hpp_key,
-    'order_id' => $order_id,
-    'charge_total' => $_SESSION['price'],
-    'hpp_preload' => '',
-    'cust_id' => $_SESSION['user_id'],
-    'email' => $user_email,
-    'note' => $moneris_note
-  ];
-
-  $post_fields_str = http_build_query($post_fields);
-
-  $request = curl_init();
-  curl_setopt($request, CURLOPT_URL, $moneris_url);
-  curl_setopt($request, CURLOPT_POST, true);
-  curl_setopt($request, CURLOPT_POSTFIELDS, $post_fields_str);
-  curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-  $response = curl_exec($request);
-
-  if ($moneris_log) {
-    error_log("\n==== preload response begin ====\n", 3, $moneris_log_file);
-    error_log($response, 3, $moneris_log_file);
-    error_log("\n==== preload response end ======\n", 3, $moneris_log_file);
-  }
-
-  //
-  // parse preload response and redirect to Moneris HPP using ticket
-  //
-  $xml = new SimpleXMLElement($response);
-  $resp_ticket = $xml->ticket;
-  $resp_hpp_id = $xml->hpp_id;
-  $resp_order_id = $xml->order_id;
-  $resp_code = $xml->response_code;
-
-  if (intval($resp_code) >= 50) {
-    $mesg = "Unable to preload Moneris data";
-    $redirect_url = 'error.php?mesg=' . urlencode($mesg);
-    header("Location: $redirect_url");
-    exit;
-  }
-  if ($resp_order_id != $order_id) {
-    $mesg = "Error preloading Moneris data";
-    $redirect_url = 'error.php?mesg=' . urlencode($mesg);
-    header("Location: $redirect_url");
-    exit;
-  }
-
-  $redirect_url = $moneris_url . '?hpp_preload=&ticket=' . $resp_ticket . '&hpp_id=' . $resp_hpp_id;
-  header("Location: $redirect_url");
-
+$result = moneris_preload($order_id, $_SESSION['price']);
+$ticket = $result[0];
+$error = $result[1];
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+ <meta charset="utf-8" />
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+ <title>Moneris Checkout Demo</title>
+</head>
+<body>
+<div class="banner">
+<a href="https://www.uvic.ca/library/"><img src="https://devwebapp.library.uvic.ca/alma/sms/image/thumbnail_01VIC_INST-01UVIC.png" width="180" alt="University of Victoria Libraries"></a>
+</div>
+<p>
+<h4>Moneris Checkout</h4>
+</p>
+<div id="outerDiv" style="width:400px;height=300px">
+<div id="monerisCheckout"></div>
+<script src="https://gateway.moneris.com/chktv2/js/chkt_v2.00.js"></script>
+<script>
+  var co = new monerisCheckout();
+  co.setMode("prod");
+  co.setCheckoutDiv("monerisCheckout");
+  co.startCheckout("<?php echo $ticket ?>");
+
+  var pageload_handler = function(data) {
+    console.log("preload", data);
+  };
+
+  var cancel_handler = function(data) {
+    console.log("cancel", data);
+    co.closeCheckout();
+    window.location.replace("/3dprint/customer-dashboard.php");
+  };
+
+  var error_handler = function(data) {
+    console.log("error", data);
+    const json = JSON.parse(data);
+    co.closeCheckout();
+    window.location.replace("/3dprint/customer-dashboard.php");
+  };
+
+  var receipt_handler = function(data) {
+    const json = JSON.parse(data);
+    console.log("receipt", json);
+    var req = new XMLHttpRequest();
+    req.overrideMimeType("application/json");
+    req.open("GET", "/3dprint/moneris/receipt.php?amount=<?php echo $_SESSION['price'] ?>&order_id=<?php echo $order_id ?>&ticket=" + json.ticket, true);
+    req.onload = function() { console.log("receipt response", this.jsonResponse); }
+    req.send(null);
+  };
+
+  var complete_handler = function(data) {
+    window.location.replace("/3dprint/customer-dashboard.php");
+  };
+
+  co.setCallback("page_loaded", pageload_handler);
+  co.setCallback("cancel_transaction", cancel_handler);
+  co.setCallback("error_event", error_handler);
+  co.setCallback("payment_receipt", receipt_handler);
+  co.setCallback("payment_complete", complete_handler);
+</script>
+</body>
+</html>
+
