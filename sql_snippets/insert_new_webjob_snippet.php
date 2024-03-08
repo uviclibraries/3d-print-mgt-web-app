5222f162f8437d36fb248f5d564ba2e939abbf66
chloe-farr <chloe.l.farr@gmail.com>
2024-03-08 13:20:39 -0800

    add fields to sql update

--- sql_snippets/insert_new_webjob_snippet.php
+++ sql_snippets/insert_new_webjob_snippet.php
@@ -1,7 +1,9 @@
 <?php
 
-	$stmt = $conn->prepare("INSERT INTO web_job (netlink_id, job_name, job_purpose, academic_code, course_due_date, submission_date, status) VALUES (:netlink_id, :job_name, :job_purpose, :academic_code, :course_due_date,:submission_date, :job_status)");
+	$stmt = $conn->prepare("INSERT INTO web_job (netlink_id, job_name, job_purpose, academic_code, course_due_date, submission_date, status, parent_job_id) VALUES (:netlink_id, :job_name, :job_purpose, :academic_code, :course_due_date,:submission_date, :job_status, :parent_job_id)");
 
+	$default_parent = 0;
+	$default_is_parent = 0;
 	$stmt->bindParam(':netlink_id', $user);
 	$stmt->bindParam(':job_name', $_POST["job_name"]);
 	$stmt->bindParam(':job_purpose', $_POST["job_purpose"]);
@@ -9,6 +11,7 @@
 	$stmt->bindParam(':course_due_date',$_POST["academic_deadline"]);
 	$stmt->bindParam(':job_status', $status);
 	$stmt->bindParam(':submission_date', $current_date);
+	$stmt->bindParam(':parent_job_id', $default_parent);
 
     $good_statement &= $stmt->execute();
 	
