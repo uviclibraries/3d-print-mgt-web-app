4a477df9e73ffd021099137c7117f5e9c4ae4e0d
chloe-farr <chloe.l.farr@gmail.com>
2024-03-15 09:38:18 -0700

    Update admin_update_multiple_status_snippet.php

--- sql_snippets/admin_update_multiple_status_snippet.php
+++ sql_snippets/admin_update_multiple_status_snippet.php
@@ -10,6 +10,19 @@
 
       $checkedIDs_sql = implode(',', $checked_jobs);//to create comma separated list for update query
 
+      $oob_link_statuses = array('pending payment', 'paid', 'printing', 'completed', 'delivered', 'cancelled', 'archived');
+      $placeholder_statuses = rtrim(str_repeat('?,', count($oob_link_statuses)), ',');
+
+      if (isset($_POST['set-children-checkbox']) && $_POST['set-children-checkbox'] == 'set_children') {
+        $new_parent_job_id = $job['id'];
+        //change parent_job_ids of all jobs that have been selected when <set-children-checkbox> has also been checked
+
+        //note that if any job that shouldnt be set as a child is selected by the user, the set-children-checkbox will disappear
+        $link_children = $conn->prepare("UPDATE web_job SET parent_job_id = $new_parent_job_id WHERE id IN ($checkedIDs_sql)");
+
+        $link_children->execute();
+      }
+      
       if(isset($_POST['set-statuses-checkbox']) && $_POST['set-statuses-checkbox'] == 'set_statuses' && isset($_POST["status"])){
 
         $status_updates = $conn->prepare("UPDATE web_job SET status = :status, priced_date = :priced_date, paid_date = :paid_date, printing_date = :printing_date, completed_date = :completed_date, delivered_date = :delivered_date, archived_date=:archived_date, priced_signer=:priced_signer,  paid_signer= :paid_signer, printing_signer=:printing_signer, completed_signer=:completed_signer, delivered_signer=:delivered_signer, hold_date = :hold_date, hold_signer= :hold_signer, cancelled_date=:cancelled_date, cancelled_signer = :cancelled_signer, archived_signer = :archived_signer WHERE id IN ($checkedIDs_sql)");
@@ -35,32 +48,26 @@
         $status_updates-> bindParam(':archived_signer',$n_archived);
         $status_updates->execute();
       }
-
-      if (isset($_POST['set-children-checkbox']) && $_POST['set-children-checkbox'] == 'set_children') {
-      $new_parent_job_id = $job['id'];
-      //change parent_job_ids of all jobs that have been selected when <set-children-checkbox> has also been checked
-      $new_children = "UPDATE web_job SET parent_job_id = $new_parent_job_id WHERE id IN ($checkedIDs_sql) AND is_parent = 0";
-
-        $link_children = $conn->prepare($new_children);
-
-        // Ensure $job['id'] is defined and valid
-         
-        // $link_children->bindParam(':parent_job_id', $new_parent_job_id, PDO::PARAM_INT);
-
-        $link_children->execute();
+      $empty_parent = 0;
+      if (isset($_POST['set-statuses-checkbox']) && $_POST['set-statuses-checkbox'] == 'set_statuses' && isset($_POST["status"]) && $_POST["status"] == 'cancelled') {
+        echo('cancelled jobs');
+        //change parent_job_ids of all jobs to 0 that have been selected when <unlink-children-checkbox> has also been checked
+        $unlink_children = $conn->prepare("UPDATE web_job SET parent_job_id = :empty_parent WHERE parent_job_id = {$prev_parent_id} AND id IN ($checkedIDs_sql)");
+        
+        $unlink_children->bindParam(':empty_parent', $empty_parent);
+        $unlink_children->execute();
       }
 
-      if (isset($_POST['unlink-children-checkbox']) && $_POST['unlink-children-checkbox'] == 'unlink_children') {
-        $new_parent_job_id = 0;
+        if (isset($_POST['unlink-children-checkbox']) && $_POST['unlink-children-checkbox'] == 'unlink_children') {
+        echo('abandoned jobs<br>');
+        print_r($checkedIDs_sql.'<br>');
+        echo($prev_parent_id.'<br>');
+
+
         //change parent_job_ids of all jobs to 0 that have been selected when <unlink-children-checkbox> has also been checked
-        $un_children = "UPDATE web_job SET parent_job_id = $new_parent_job_id WHERE id IN ($checkedIDs_sql) AND is_parent = 0";
-
-        $unlink_children = $conn->prepare($un_children);
-
-        // Ensure $job['id'] is defined and valid
-         
-        // $link_children->bindParam(':parent_job_id', $new_parent_job_id, PDO::PARAM_INT);
-
+        $unlink_children = $conn->prepare("UPDATE web_job SET parent_job_id = :empty_parent WHERE parent_job_id = :this_job AND id IN ($checkedIDs_sql)");
+        $unlink_children->bindParam(':empty_parent', $empty_parent);
+        $unlink_children->bindParam(':this_job',$job['id']);
         $unlink_children->execute();
       }
     }
