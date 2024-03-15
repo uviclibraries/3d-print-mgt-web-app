db8686d9dae04e7191de3805ea1ed227d5a89830
chloe-farr <chloe.l.farr@gmail.com>
2024-03-15 09:36:42 -0700

    checks for if any jobs with parent_id set to the job's referenced parent then change is_parent

--- sql_snippets/admin_remove_prev_parent-snippet.php
+++ sql_snippets/admin_remove_prev_parent-snippet.php
@@ -1,29 +1,20 @@
 <?php
 
-//checks if the job was previously assigned a parent. If true, and if parent changed, loops through user_active_jobs to check if previous parent is still a parent of other jobs. If false, changes prev parent to is_parent=false.
-//Run last to allow for the current job's parent_id to have changed in db.
+if($prev_parent_id != 0){
+    echo($prev_parent_id);
+    $stm = $conn->prepare("SELECT COUNT(*) FROM web_job WHERE parent_job_id = :parent_job_id");
+    $stm->bindParam(':parent_job_id', $prev_parent_id);
+    $stm->execute();
+    print_r($stm);
+    $num_children = $stm->fetchColumn();
+    echo($num_children);
 
-//still_parent is true if job had a parent previously and parent_job_id wasn't set to 0, false if wasn't previously
-$n_parent = false;
-if(isset($_POST['checked_jobs']) && $_POST['checked_jobs'] && count($_POST['checked_jobs'])>0){
-    $checked_jobs = $_POST['checked_jobs'];
-    $compare_linked = array_diff($linked_jobs, $checked_jobs);
-
-    forEach ($compare_linked as $linked) {
-        if($linked['parent_job_id']){
-            $n_parent = true;
-            break;
-        }
-        else{$n_parent = false;}
-    }
-
-    //check if unlink checkbox is visible and was checked
-    if(!$n_parent){
-        $parent = $prev_parent_id;
-        $set_isParent = $conn->prepare("UPDATE web_job SET is_parent = FALSE WHERE id = {$parent}");
+    if($num_children == 0){
+        $is_parent = false;
+        $set_isParent = $conn->prepare("UPDATE web_job SET is_parent = :new_is_parent WHERE id = :prev_parent");
+        $set_isParent->bindParam(':new_is_parent', $is_parent, PDO:: PARAM_BOOL);
+        $set_isParent->bindParam(':prev_parent',$prev_parent_id);
         $set_isParent->execute();
-
     }
 }
-
 ?>
