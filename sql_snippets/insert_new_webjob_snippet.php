efc8dc51e69343647b796f090ebc40adf6e0acbe
chloe-farr <chloe.l.farr@gmail.com>
2024-03-28 11:21:37 -0700

    Bound fields as empty strings that would otherwise be null
    to acommondate php8 string type entry

--- sql_snippets/insert_new_webjob_snippet.php
+++ sql_snippets/insert_new_webjob_snippet.php
@@ -1,9 +1,10 @@
 <?php
 
-	$stmt = $conn->prepare("INSERT INTO web_job (netlink_id, job_name, job_purpose, academic_code, course_due_date, submission_date, status, parent_job_id) VALUES (:netlink_id, :job_name, :job_purpose, :academic_code, :course_due_date,:submission_date, :job_status, :parent_job_id)");
+	$stmt = $conn->prepare("INSERT INTO web_job (netlink_id, job_name, job_purpose, academic_code, course_due_date, submission_date, status, priced_signer, hold_signer, paid_signer, printing_signer, completed_signer, delivered_signer, cancelled_signer, parent_job_id) VALUES (:netlink_id, :job_name, :job_purpose, :academic_code, :course_due_date,:submission_date, :job_status, :priced_signer, :hold_signer, :paid_signer, :printing_signer, :completed_signer, :delivered_signer, :cancelled_signer, :parent_job_id)");
 
 	$default_parent = 0;
 	$default_is_parent = 0;
+	$empty_signer = "";
 	$stmt->bindParam(':netlink_id', $user);
 	$stmt->bindParam(':job_name', $_POST["job_name"]);
 	$stmt->bindParam(':job_purpose', $_POST["job_purpose"]);
@@ -11,7 +12,15 @@
 	$stmt->bindParam(':course_due_date',$_POST["academic_deadline"]);
 	$stmt->bindParam(':job_status', $status);
 	$stmt->bindParam(':submission_date', $current_date);
+	$stmt->bindParam(':priced_signer', $empty_signer);
+	$stmt->bindParam(':hold_signer', $empty_signer);
+	$stmt->bindParam(':paid_signer', $empty_signer);
+	$stmt->bindParam(':printing_signer', $empty_signer);
+	$stmt->bindParam(':completed_signer', $empty_signer);
+	$stmt->bindParam(':delivered_signer', $empty_signer);
+	$stmt->bindParam(':cancelled_signer', $empty_signer);
 	$stmt->bindParam(':parent_job_id', $default_parent);
+
 
     $good_statement &= $stmt->execute();
 	
